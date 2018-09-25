6d353ea59f1a71af5093fb2d1089fd5d776e9e27
Lorenzo <Lorenzo@lorenzos-mbp.homenet.telecomitalia.it>
2018-09-25 12:05:41 +0200

    added database.txt e SQL_DB.txt

--- src/main/java/it/unitn/aa1718/webprogramming/geolists/servlets/ServletRoot.java
+++ src/main/java/it/unitn/aa1718/webprogramming/geolists/servlets/ServletRoot.java
@@ -11,16 +11,6 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "ServletRoot", urlPatterns = {"/"})
 public class ServletRoot extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods. Every time this servlet is reached by a GET or a POST request
-     * it invokes this function
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
@@ -38,39 +28,19 @@ public class ServletRoot extends HttpServlet {
         }
     }
 
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
     @Override
     public String getServletInfo() {
         return "This is the servlet that handles requests in the main root of the server";
