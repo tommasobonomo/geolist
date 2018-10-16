d162a545d09cbeb77ed54bcc331792152172ff61
Mattia Molon <mmattia06@gmail,com>
2018-10-16 15:47:56 +0200

    Added some comments to ServletRoot to understand better the code

--- sql_query/populatedb.sql
+++ sql_query/populatedb.sql
@@ -1,13 +1,13 @@
 INSERT INTO GEODB.USERS(USERNAME,"NAME",LASTNAME,EMAIL,IMAGE, PASSWORD, "ADMIN")
 VALUES ('rappasta', 'Lorenzo', 'Framba', 'Lorenzoframba@gmail.com','image','pasta',TRUE),
        ('gesuSesso','Mattia', 'Molon','Molon@gmail.com','IMAGE','c123456',FALSE),
-       ( 'minestrone','Tommaso','Bonomo','Tommasoni394@gmail.com','IMAGEN','qwerty',FALSE),
+       ('minestrone','Tommaso','Bonomo','Tommasoni394@gmail.com','IMAGEN','qwerty',FALSE),
        ('XDSNIPERXD','Leonardo','Remondini', 'Remoromolo@gmail.com','IMAGEN', 'sdfghj',FALSE),
        ('piediPiatti','Giorgio','Segalla','Giorgionelo@gmail.com','IMAGEN', 'sdfghj',FALSE),
        ('Maialino69','Marta','Tonnona', 'Martinasesso@gmail.com','IMAGEN', 'sdfghjekrf',FALSE),
        ('Fratak','Elia','Salame','piselloni@gmail.com','IMAGEN', 'texyckv',FALSE),
-       ( 'OrcoBue','Luca', 'Rossi', 'marione@gmail.com','IMAGEN', 'lieuwhf',FALSE),
-       ( 'Iphonneeee','Nicola','Nicoli', 'naso@gmail.com','IMAGEN', 'fieguwkh',FALSE),
+       ('OrcoBue','Luca', 'Rossi', 'marione@gmail.com','IMAGEN', 'lieuwhf',FALSE),
+       ('Iphonneeee','Nicola','Nicoli', 'naso@gmail.com','IMAGEN', 'fieguwkh',FALSE),
        ('Bottiglia','Marco','Bestioni', 'tomcat@gmail.com','IMAGEN', 'feluakh',FALSE);
 
 
