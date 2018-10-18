f5bfefe3dece98870cf3c3fe73d6b17466f30ac2
Tommaso Bonomo <tommaso.bonomo.97@gmail.com>
2018-10-18 22:28:45 +0200

    Added DAOs for Item and Compose
    Compose is a bit custom because it's a many to many relation

--- src/main/java/it/unitn/aa1718/webprogramming/geolists/database/ItemDAO.java
+++ src/main/java/it/unitn/aa1718/webprogramming/geolists/database/ItemDAO.java
@@ -1,11 +1,12 @@
+package it.unitn.aa1718.webprogramming.geolists.database;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package it.unitn.aa1718.webprogramming.geolists.database;
 
-import it.unitn.aa1718.webprogramming.geolists.database.models.ProductList;
+import it.unitn.aa1718.webprogramming.geolists.database.models.Item;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -15,27 +16,28 @@ import java.util.List;
 import java.util.Optional;
 
 /**
- * DAO pattern for the List relation
+ * Implementation of DAO pattern for Item relation
  * @author tommaso
  */
-public class ProductListDAO implements CrudDao<ProductList> {
+public class ItemDAO implements CrudDao<Item>{
 
-    private ProductList createProductList(ResultSet rs) throws SQLException {
-        return new ProductList(rs.getLong("idList"),rs.getString("userCreator"), rs.getLong("idCat"),
-            rs.getString("name"),rs.getString("description"), rs.getString("image"));        
+    private Item createItem(ResultSet rs) throws SQLException {
+        return new Item(rs.getLong("idItem"), rs.getLong("idCat"), rs.getString("calorie"),
+                rs.getString("name"), rs.getString("logo"), rs.getString("note"));
     }
     
     @Override
-    public Optional<ProductList> get(long id) {
-        String query = "SELECT * FROM List AS L WHERE L.idList=" + id; 
-                
+    public Optional<Item> get(long id) {
+        
+        String query = "SELECT * FROM Item AS I WHERE I.idItem = " + id;
+        
         try {
             Connection c = Database.openConnection();
             Statement s = c.createStatement();
             ResultSet rs = s.executeQuery(query);
             
             if (rs.next()) {
-                return Optional.of(createProductList(rs));
+                return Optional.of(createItem(rs));
             } else {
                 return Optional.empty();
             }
@@ -48,8 +50,8 @@ public class ProductListDAO implements CrudDao<ProductList> {
     }
 
     @Override
-    public List<ProductList> getAll() {
-        String query = "SELECT * FROM List";
+    public List<Item> getAll() {
+        String query = "SELECT * FROM Item";
         List list = new ArrayList<>();
         
         try {
@@ -58,7 +60,7 @@ public class ProductListDAO implements CrudDao<ProductList> {
             ResultSet rs = s.executeQuery(query);
             
             while (rs.next()) {
-                list.add(createProductList(rs));
+                list.add(createItem(rs));
             }
             
             rs.close();
@@ -73,17 +75,17 @@ public class ProductListDAO implements CrudDao<ProductList> {
     }
 
     @Override
-    public void create(ProductList obj) {
+    public void create(Item obj) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public void update(long id, ProductList obj) {
+    public void update(long id, Item obj) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public void delete(ProductList obj) {
+    public void delete(Item obj) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
