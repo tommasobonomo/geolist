926c970069d945405892168a6a6e3cee88df8512
GiorgioSgl <giorgiosgl1997@gmail.com>
2018-10-26 16:48:24 +0200

    complete access.java method and constructor

--- src/main/java/it/unitn/aa1718/webprogramming/geolists/database/models/Access.java
+++ src/main/java/it/unitn/aa1718/webprogramming/geolists/database/models/Access.java
@@ -10,5 +10,28 @@ package it.unitn.aa1718.webprogramming.geolists.database.models;
  * @author tommaso
  */
 public class Access { 
+    private long idUser;
+    private long idList;
+    
+    public Access(long idUser,long idList){
+        this.idList=idList;
+        this.idUser=idUser;
+    }
+    
+    public void setIdUser(long idUser){
+        this.idUser=idUser;
+    }
+    
+    public long getIdUser(){
+        return this.idUser;
+    }
+    
+    public void setIdList(long idList){
+        this.idList=idList;
+    }
+    
+    public long getIdList(){
+        return this.idList;
+    }
     
 }
