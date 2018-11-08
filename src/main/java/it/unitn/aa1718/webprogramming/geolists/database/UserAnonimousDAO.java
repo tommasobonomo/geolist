0c06e69f0287033176b63439afe5be335a6cfe27
GiorgioSgl <giorgiosgl1997@gmail.com>
2018-11-08 15:07:23 +0100

    add method for transform in a user, to update

--- src/main/java/it/unitn/aa1718/webprogramming/geolists/database/UserAnonimousDAO.java
+++ src/main/java/it/unitn/aa1718/webprogramming/geolists/database/UserAnonimousDAO.java
@@ -5,6 +5,8 @@
  */
 package it.unitn.aa1718.webprogramming.geolists.database;
 
+import it.unitn.aa1718.webprogramming.geolists.database.models.ProductList;
+import it.unitn.aa1718.webprogramming.geolists.database.models.User;
 import it.unitn.aa1718.webprogramming.geolists.database.models.UserAnonimous;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -23,6 +25,46 @@ public class UserAnonimousDAO implements CrudDao<UserAnonimous>{
     
     private UserAnonimous createUserAnonimous(ResultSet rs) throws SQLException {
         return new UserAnonimous(rs.getLong(1),rs.getString("cookie"));
+    }
+    
+    /**
+     * transform a anonymous user in a user with his list
+     * @param ua utente anonimo da trasformare
+     * @param u utente con dati da creare nel database
+     * @return false means there is an error, true means there is no error but not that
+     * work
+     */
+    public boolean becomeUserRegister(UserAnonimous ua,User u){
+        
+        try{
+            // creo il nuovo utente nel database e gli setto il cookie dell'utente anonimo
+            UserDAO userDb = new UserDAO();
+            u.setCookie(ua.getCookie());
+            userDb.create(u);
+        
+            //prendo il nuovo utente dal database (per avere l'id)
+            User u_new= null;
+            u_new = userDb.getUser(u.getCookie()).get();
+        
+            //collego le liste dell'utente anonimo a quello delle liste del nuovo utente
+            ProductListDAO listDb = new ProductListDAO();
+            ProductList p = listDb.getListAnon(ua.getId()).get(0);  // prendo la prima e unica lista
+                                                                //dell'utente anonimo
+            p.setUserOwner(u_new.getId());
+            System.out.println(p);
+            listDb.update(p.getId(), p); // aggiorno la lista
+            listDb.updateUserAnonOwnerToNull(p.getId()); // setto a null l'anonimoowner nella tabella lista
+            
+            //rimuovo l'anonimo dalla tabella useranonymous
+            UserAnonimousDAO anoDb = new UserAnonimousDAO();
+            anoDb.delete(ua.getId());
+            
+            return true;
+        }catch(Exception e){
+            e.printStackTrace();
+            
+            return false;
+        }
     }
     
     @Override
